78178ea06990cb061a0b3d99a36c477d20aee8fd
Eujern Lim <eujern@gmail.com>
2013-09-07 18:33:09 -0400

    Init.

---
+++ risk.py
@@ -0,0 +1,53 @@
+"""Deploy Risk Evaluator"""
+
+#imports
+import sys
+
+import re
+from optparse import OptionParser
+import random
+from sh import git
+
+# constants
+REPO_DIR = "/home/eujern/Dropbox/ymudq"
+
+
+# exception classes
+# interface functions
+# classes
+
+# internal functions & classes
+
+def main():
+    usage = "Usage: %prog <prevrev> <currev>"
+    parser = OptionParser(usage=usage)
+    (options, args) = parser.parse_args()
+    if len(args) != 2:
+        parser.error("incorrect number of arguments")
+
+    print "Repository at: " + REPO_DIR
+    repo = git.bake(_cwd=REPO_DIR, _tty_out=False)
+
+    # just to verify
+    repo.status()
+
+    print "Repository verified."
+
+    commits  = repo.log(args[0]  + '..' + args[1], '--pretty=format:%h', '--no-color').splitlines()
+
+    print "Number of revisions: " + `len(commits)`
+
+    count = 0
+
+    for revision in commits:
+        diffs = repo.show(revision, '--oneline').splitlines()
+        # skip first 5 lines
+        for line in diffs[5:]:
+            if line.startswith("-") or line.startswith("+"):
+                count += 1
+
+    print "Lines changed: " + `count`
+
+if __name__ == '__main__':
+    status = main()
+    sys.exit(status)
